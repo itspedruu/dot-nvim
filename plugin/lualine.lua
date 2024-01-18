6368c2c9000b36fc83da564baa265a15f41fa3e0
Pedro Pinto <pedropinto.01@hotmail.com>
2024-01-18 23:34:37 +0000

    changed lsp message in lualine

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -150,21 +150,27 @@ ins_left {
 ins_left {
   -- Lsp server name .
   function()
-    local msg = 'No Active Lsp'
+    local msg = 'No LSP'
     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
     local clients = vim.lsp.get_active_clients()
     if next(clients) == nil then
       return msg
     end
+	local names = {}
+	local count = 0
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
       if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
+		table.insert(names, client.name)
+		count = count + 1
       end
     end
+
+	if count > 0 then
+		return table.concat(names, " + ")
+	end
     return msg
   end,
-  icon = ' LSP:',
   color = { fg = '#ffffff', gui = 'bold' },
 }
 
