06110a530d29e860c5a44edcf10d9dec2be6ed39
Pedro Pinto <pedropinto.01@hotmail.com>
2025-09-23 22:57:11 +0100

    feat: gitsigns mappings

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -1,1 +1,8 @@
-require('gitsigns').setup()
+require('gitsigns').setup{
+	on_attach = function()
+		local gitsigns = require('gitsigns')
+
+		vim.keymap.set('n', '<leader>b', gitsigns.toggle_current_line_blame)
+		vim.keymap.set('n', '<leader>hd', gitsigns.diffthis)
+	end
+}
